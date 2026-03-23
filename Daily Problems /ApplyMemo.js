e7d7eace077f86fd7967477bc771c8382be577e0
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-03-23 19:39:51 +0530

    solved: Memo function for cashing function calling with same arguments

--- Daily Problems /ApplyMemo.js
+++ Daily Problems /ApplyMemo.js
@@ -2,16 +2,14 @@ const memo = (func) => {
   let map = new Map();
   let result = 0;
   return (...args) => {
-    // console.log(args);
-    // console.log(func.name);
-    console.log(map.has(args));
-    if (map.has(args)) {
+    let key = args.join("");
+    if (map.has(key)) {
       console.log("From Cash");
-      result = map.get(args);
+      result = map.get(key);
     } else {
       console.log("Calculate new Result");
       result = func(...args);
-      map.set(args, result);
+      map.set(key, result);
     }
     return result;
   };
@@ -22,5 +20,8 @@ const func = (arg1, arg2) => {
 
 const memoed = memo(func);
 
-console.log(memoed(1, 2));
-console.log(memoed(1, 2));
+console.log(memoed(1, 2)); //Calculate new Result
+console.log(memoed(1, 3)); //Calculate new Result
+console.log(memoed(1, 4)); //Calculate new Result
+console.log(memoed(1, 2)); //From Cash
+console.log(memoed(1, 4)); //From Cash
