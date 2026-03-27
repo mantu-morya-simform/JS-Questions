dddce62867deea6a3a602343f18349f0c2e200e5
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-03-27 18:34:50 +0530

    solved: check equality of nested object and arrays

--- Daily Problems /checkIsObjEqual.js
+++ Daily Problems /checkIsObjEqual.js
@@ -15,8 +15,12 @@
 // a === b; // false
 */
 
-const isEqual = (a, b) => {
+const isEqual = (a, b, weakMap = new WeakMap()) => {
+  if (a === null || b === null) return a === b;
   if (a === b) return true;
+
+  if (weakMap.has(a)) return weakMap.get(a) == b;
+  weakMap.set(a, b);
 
   if (Array.isArray(a) && Array.isArray(b)) {
     if (a.length !== b.length) {
@@ -24,19 +28,23 @@ const isEqual = (a, b) => {
     }
 
     for (let i = 0; i < a.length; i++) {
-      if (!isEqual(a[i], b[i])) return false;
+      if (!isEqual(a[i], b[i], weakMap)) return false;
     }
     return true;
   }
 
   if (typeof a === "object" && typeof b === "object") {
-    let keysA = Object.keys(a);
-    let keysB = Object.keys(b);
+    let keysA = Object.keys(a).sort();
+    let keysB = Object.keys(b).sort();
 
     if (keysA.length !== keysB.length) return false;
 
     for (let key of keysA) {
-      if (!isEqual(a[key], b[key])) return false;
+      if (!keysB.includes(key)) return false;
+    }
+
+    for (let key of keysA) {
+      if (!isEqual(a[key], b[key], weakMap)) return false;
     }
     return true;
   }
@@ -46,9 +54,23 @@ const isEqual = (a, b) => {
 
 const a = { a: "bfe" };
 const b = { a: "bfe" };
+console.log(isEqual(a, b));
 
 const c = [1, a, "4"];
 const d = [1, b, "4"];
-
-console.log(isEqual(a, b));
 console.log(isEqual(c, d));
+
+const e = { a: "bfe", c: "kpe" };
+const f = { a: "bfe", d: "kpe" };
+console.log(isEqual(e, f));
+
+const g = null;
+const h = null;
+console.log(isEqual(g, h));
+
+const i = {};
+i.self = i;
+
+const j = {};
+j.self = j;
+console.log(isEqual(i, j));
