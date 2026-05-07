12c410b42f8f9e646a8ba4401e427c2b2d183647
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-05-07 18:29:31 +0530

    feat: initialize Element Builder project with TypeScript and Vite
    - Add package.json with scripts for development, build, and preview
    - Create favicon.svg and icons.svg for application branding
    - Include hero.png and TypeScript logo as assets
    - Implement ElementBuilder class for dynamic HTML element creation
    - Add tsconfig.json for TypeScript configuration
    - Create style.css for styling the application

---
+++ TS Questions/Element Builder/src/main.ts
@@ -0,0 +1,50 @@
+// Implement ElementBuilder that allows:
+
+/*
+const div = new ElementBuilder("div")
+  .addClass("container")
+  .setAttribute("id", "main")
+  .addChild("p", "Hello World")
+  .style("color", "blue")
+  .build();
+  */
+
+class ElementBuilder {
+  private element: HTMLElement;
+  constructor(tag: string) {
+    this.element = document.createElement(tag);
+  }
+
+  addClass(className: string) {
+    this.element.classList.add(className);
+    return this;
+  }
+
+  setAttribute(attributeName: string, value: string) {
+    this.element.setAttribute(attributeName, value);
+    return this;
+  }
+
+  addChild(tageType: string, content: string) {
+    let childElement = document.createElement(tageType);
+    childElement.textContent = content;
+    this.element.appendChild(childElement);
+    return this;
+  }
+
+  style(styleType: string, styleProp: string) {
+    this.element.style.setProperty(styleType, styleProp);
+    return this;
+  }
+
+  build() {
+    return this.element;
+  }
+}
+
+const div = new ElementBuilder("div")
+  .addClass("container")
+  .setAttribute("id", "main")
+  .addChild("p", "Hello World")
+  .style("color", "blue")
+  .build();
